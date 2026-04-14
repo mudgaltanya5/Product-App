6e0e810523e79dc7c38fb4127a6c131684b2a245
Tanya <mudgaltanya5@gmail.com>
2026-04-15 00:35:46 +0530

    final fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import ProductList from "./components/ProductList";
 import SearchBar from "./components/SearchBar";
-import Loader from "./components/Loader";
 import Error from "./components/Error";
 
 function App() {
@@ -11,32 +10,38 @@ function App() {
   const [error, setError] = useState("");
 
   useEffect(() => {
-    fetch("https://fakestoreapi.com/products")
-      .then((res) => {
+    const fetchProducts = async () => {
+      try {
+        const res = await fetch("https://fakestoreapi.com/products");
+
         if (!res.ok) {
           throw new Error("Server error");
         }
-        return res.json();
-      })
-      .then((data) => {
+
+        const data = await res.json();
         setProducts(data);
         setFilteredProducts(data);
-        setLoading(false);
-      })
-      .catch((err) => {
+      } catch (err) {
         console.error("Error:", err);
         setError("Failed to fetch data");
+      } finally {
         setLoading(false);
-      });
+      }
+    };
+
+    fetchProducts();
   }, []);
 
   return (
     <div className="container">
       <h2 className="text-center mt-3">Product Explorer</h2>
 
-      <SearchBar products={products} setFilteredProducts={setFilteredProducts} />
+      <SearchBar
+        products={products}
+        setFilteredProducts={setFilteredProducts}
+      />
 
-      {loading && <h3>Loading...</h3>}
+      {loading && <h3 className="text-center mt-4">Loading...</h3>}
 
       {error && <Error message={error} />}
 
